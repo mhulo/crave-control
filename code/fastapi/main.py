76c315a47a417205a649be12e79f0898c2b47a5e
Michael <Hulo>
2020-10-06 17:20:27 +1100

    more fastapi setup

--- code/fastapi/main.py
+++ code/fastapi/main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -9,6 +10,20 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/api/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+@app.get("/api/items/{item_id}/")
+def read_item(item_id: str, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/api/aaa/", response_class=HTMLResponse)
+async def read_html():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
